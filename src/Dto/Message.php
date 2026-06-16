833c22865db8c8ac869c498b154afa0e1f5e5e94
demettriss <demettriss@mail.ru>
2026-06-16 12:23:01 +0300

    ...

--- src/Dto/Message.php
+++ src/Dto/Message.php
@@ -9,11 +9,8 @@ class Message extends BaseObject
         'sender' => Sender::class,
         'body' => Body::class,
     ];
-    public function recipient(): \Closure
-    { return $this->get('recipient'); }
-    public function sender(): \Closure
-    { return $this->get('sender'); }
-    public function body(): \Closure
-    { return $this->get('body'); }
+    public function recipient(): ?Recipient { return $this->get('recipient'); }
+    public function sender(): ?Sender { return $this->get('sender'); }
+    public function body(): ?Body { return $this->get('body'); }
     public function timestamp(): ?int { return $this->getInt('timestamp'); }
 }
--- src/Dto/UpdateItem.php
+++ src/Dto/UpdateItem.php
@@ -7,8 +7,7 @@ class UpdateItem extends BaseObject
     protected static array $schema = [
         'message' => Message::class,
     ];
-    public function message(): \Closure
-    { return $this->get('message'); }
+    public function message(): ?Message { return $this->get('message'); }
     public function timestamp(): ?int { return $this->getInt('timestamp'); }
     public function userLocale(): ?string { return $this->getString('user_locale'); }
     public function updateType(): ?string { return $this->getString('update_type'); }
