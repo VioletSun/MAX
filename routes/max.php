3c78d11be5fd517b7864472649dae412f2552469
demettriss <demettriss@mail.ru>
2026-06-28 16:24:27 +0300

    Routes & MaxController & views fish

--- routes/max.php
+++ routes/max.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use VioletSun\MAX\Controllers\MaxController;
 
 
-Route::prefix('max')->group(function () {
-    Route::prefix('v1')->group(function () {
+Route::prefix('v1')->group(function () {
+    Route::prefix('max')->group(function () {
         Route::get('webhook', [MaxController::class, 'webhook'])->name('api.max.webhook');
         Route::get('/', [MaxController::class, 'index'])->name('api.max.index');
         Route::post('action', [MaxController::class, 'action'])->name('api.max.action');
