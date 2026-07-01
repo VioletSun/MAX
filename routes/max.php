d351ac8ce7259203e74e07b48bc636af8c81c42e
demettriss <demettriss@mail.ru>
2026-07-02 02:23:07 +0300

    ...

--- routes/max.php
+++ routes/max.php
@@ -6,8 +6,8 @@ use VioletSun\MAX\Controllers\MaxController;
 
 Route::prefix('v1')->group(function () {
     Route::prefix('max')->group(function () {
-        Route::get('webhook', [MaxController::class, 'webhook'])->name('api.max.webhook');
-        Route::get('/', [MaxController::class, 'index'])->name('api.max.index');
+        Route::any('webhook', [MaxController::class, 'webhook'])->name('api.max.webhook');
+        Route::any('/', [MaxController::class, 'index'])->name('api.max.index');
         Route::post('action', [MaxController::class, 'action'])->name('api.max.action');
     });
 });
