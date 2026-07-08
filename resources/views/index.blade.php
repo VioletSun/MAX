b5ef1837dd71da56d2e4f1d02c829987ec79b16d
demettriss <demettriss@mail.ru>
2026-07-08 14:35:31 +0300

    Mini APP

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+{{--    <meta name="viewport" content="width=device-width, initial-scale=1">--}}
     <meta name="viewport" content="user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1,width=device-width,interactive-widget=resizes-visual,viewport-fit=cover">
 
     <meta name="app-token" content="{{ max_make_app_token() }}">
