a8cc7d52222affc64c64853a398271dc3a1b209b
demettriss <demettriss@mail.ru>
2026-07-09 14:11:10 +0300

    Markup

--- src/Objects/Message/Markup.php
+++ src/Objects/Message/Markup.php
@@ -4,16 +4,29 @@ declare(strict_types=1);
 
 namespace VioletSun\MAX\Objects\Message;
 
+use VioletSun\MAX\Enums\MarkupTypeEnum;
 use VioletSun\MAX\Support\BaseObject;
 
+/**
+ * @property int $from
+ * @property int $length
+ * @property string $type
+ * @property string|null $url
+ * @property string|null $user_link
+ * @property int|null $user_id
+ */
 final class Markup extends BaseObject
 {
     public static function fromArray(array $data): static
     {
-        return new self([
-            'from' => $data['from'] ?? null,
-            'length' => $data['length'] ?? null,
-            'type' => $data['type'] ?? null,
-        ]);
+        $datum = [];
+        foreach ($data as $key => $value) {
+            if ($key === 'type') {
+                $datum[$key] = MarkupTypeEnum::tryFrom($value);
+            } else {
+                $datum[$key] = $value;
+            }
+        }
+        return new self($datum);
     }
 }
